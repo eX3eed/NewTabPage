7a53004c7cf3feb3a6ef483f49f91c04317ee8e6
Danil <captainaxe1337@gmail.com>
2023-07-19 14:42:07 +0300

    add: getting emogies from fixed list

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let getTime = document.getElementById("time");
+let getSmile = document.getElementById("smile");
+let random = Math.floor(Math.random() * 7)
+
+let smileList = {
+  0: "˚ʚ(´•◡•`)ɞ˚",	
+  1: "(￢‿￢ )",
+  2: "ヽ(・∀・)ﾉ",
+  3: "。。。ミヽ(。＞＜)ノ",
+  4: "ฅ(•ㅅ•❀)ฅ",
+  5: "( ͡° ͜ʖ ͡°)",
+  6: "( ･ิ ͜ʖ ･ิ)"
+}
 
 let options = {
   hour: "numeric",
@@ -12,3 +24,4 @@ setInterval(() => {
   getTime.innerHTML = time;
 }, 1);
 
+getSmile.innerHTML = smileList[random];
